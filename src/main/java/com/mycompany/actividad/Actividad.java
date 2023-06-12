552795d993a3e07206e5a4a375eba9ed2a30c8b4
jean carlo <jeanfajardo83@gmail.com>
2023-06-11 23:55:19 -0500

    initial commit

---
+++ src/main/java/com/mycompany/actividad/Actividad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad;
+
+/**
+ *
+ * @author jean
+ */
+public class Actividad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
